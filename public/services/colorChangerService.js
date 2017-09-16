32bbd8ce7da1e5edda2db80c8ccad29151a4f4d8
Borys Tymiński <borys00@amorki.pl>
2017-09-16 22:44:17 +0200

    converter service structure

--- public/services/colorChangerService.js
+++ public/services/colorChangerService.js
@@ -6,11 +6,35 @@
         .service('colorChangerService', Service);
 
     //$inject for minify issue
-    Service.$inject = ['colorCheckerService'];
+    Service.$inject = [];
 
-    function Service(colorCheckerService) {
-        this.colorChange = function() {
-            
+    function Service() {
+        this.colorChange = {
+            fromHEX: {
+                toHSL: function() {
+
+                },
+                toRGB: function() {
+
+                },
+                prepareAndExecute: function(color_field) {
+                    // preparing
+
+                    //this.toRGB(preparedValue);
+                    //this.toHSL(preparedValue);
+                }
+            },
+            fromRGB: {
+                toHSL: function(r, g, b) {
+
+                },
+                toHEX: {
+
+                },
+                prepareAndExecute: function(color_field) {
+
+                }
+            }
         }
     }
 })();
