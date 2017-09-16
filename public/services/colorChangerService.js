611726ea1407710f737a94a3705993c6445a8df8
Borys Tymiński <borys00@amorki.pl>
2017-09-16 23:06:17 +0200

    RGB to HSL method

--- public/services/colorChangerService.js
+++ public/services/colorChangerService.js
@@ -26,7 +26,33 @@
             },
             fromRGB: {
                 toHSL: function(r, g, b) {
+                    r /= 255, g /= 255, b /= 255;
+                    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+                    var h, s, l = (max + min) / 2;
 
+                    if(max == min){
+                        h = s = 0; // achromatic
+                    } else {
+                        var d = (max - min);
+                        s = l >= 0.5 ? d / (2 - (max + min)) : d / (max + min);
+                        switch (max) {
+                            case r:
+                                h = ((g - b) / d + 0)*60;
+                                break;
+                            case g:
+                                h = ((b - r) / d + 2)*60;
+                                break;
+                            case b:
+                                h = ((r - g) / d + 4)*60;
+                                break;
+                            }
+                    }
+
+                    console.log('Hue = '+h);
+                    console.log('Saturation = '+s);
+                    console.log('Lightness = '+l);
+
+                    return [h, s, l];
                 },
                 toHEX: {
                     supportFunc: function(number) {
