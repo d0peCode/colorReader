05aa2b69f76c4b85677772521c70d1e2b9b6137b
Borys Tyminski <borys00@amorki.pl>
2017-09-24 16:52:00 +0200

    using emitted event to gather info from service

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -6,13 +6,18 @@
         .controller('mainCtrl', Controller);
 
     //$inject for minify issue
-    Controller.$inject = ['$scope', 'colorCheckerService'];
+    Controller.$inject = ['$scope', '$rootScope', 'colorCheckerService'];
 
-    function Controller($scope, colorCheckerService) {
+    function Controller($scope, $rootScope, colorCheckerService) {
         var vm = $scope;
         vm.fireColorReader = function() {
             console.log('im in controller fireColorReader function working');
             colorCheckerService.colorCheck(vm.color_field);
-        }
+        };
+        $rootScope.$on('colorChanged', function(event, data) {
+          console.log('colorChanged event emitted');
+          console.log('colors in others: (in controller)' + data);
+        });
+
     }
 })();
