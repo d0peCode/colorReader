a2155f393615bd2a482659cb8da9c0b741075857
Borys Tymiński <tyminski.borys@gmail.com>
2019-09-03 18:29:10 +0200

    update api user update validation

--- api/src/validations/user.validation.js
+++ api/src/validations/user.validation.js
@@ -9,5 +9,17 @@ module.exports = {
       email: Joi.string().email().required(),
       password: Joi.string().min(6).max(128).required()
     }
+  },
+  update: {
+    body: {
+      _id: Joi.any().forbidden(),
+      email: Joi.any().forbidden(),
+      password: Joi.string().min(6).max(128),
+      newPassword: Joi.string().min(6).max(128),
+      name: Joi.string().max(128),
+      activationKey: Joi.any().forbidden(),
+      active: Joi.any().forbidden(),
+      resetPasswordKey: Joi.any().forbidden()
+    }
   }
 }
