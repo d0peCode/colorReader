0128590810058865a5ef3ddd246afe7226cc0555
Borys Tymiński <tyminski.borys@gmail.com>
2019-09-06 21:57:21 +0200

    delete unused auth router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,7 +5,6 @@ const router = express.Router()
 const adminRouter = require('./api/admin.route')
 const colorRouter = require('./api/color.route')
 const userRouter = require('./api/user.route')
-const authRouter = require('./api/auth.route')
 
 // api status
 router.get('/status', (req, res) => { res.send({status: 'OK'}) })
@@ -13,6 +12,5 @@ router.get('/status', (req, res) => { res.send({status: 'OK'}) })
 router.use('/admin', adminRouter)
 router.use('/color', colorRouter)
 router.use('/user', userRouter)
-router.use('/auth', authRouter)
 
 module.exports = router
